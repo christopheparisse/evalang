8e61e52def1eba283ed2a68daf5db1d21209eb6d
Christophe Parisse <christopheparisse@gmail.com>
2021-01-12 08:55:59 +0100

    Donn├®es TCOF avec insertion des metadata (2)

---
+++ maj.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Données TCOF avec insertion des metadata (2)"
+git push origin
--- tcof/chi-trans-metaok/exemple.bat
+++
@@ -1,4 +0,0 @@
-java -cp C:\devlopt\teicorpo\teicorpo.jar fr.ortolang.teicorpo.TeiCorpo -to CLAN .\aline1_gan.tei_corpo2.xml -normalize clan -o .\aline1_gan.tei_corpo3.cha -rawline -target clan
-java -cp C:\devlopt\teicorpo\teicorpo.jar fr.ortolang.teicorpo.TeiCorpo .\aline1_gan.tei_corpo3.cha
-java -cp C:\devlopt\teicorpo\teicorpo.jar fr.ortolang.teicorpo.TeiTreeTagger -syntaxformat ref -program C:\brainstorm\syntax\bin\tree-tagger.exe -model C:\brainstorm\syntax\models\french.par .\aline1_gan.tei_corpo3.tei_corpo.xml -target ttg
-java -cp C:\devlopt\teicorpo\teicorpo.jar fr.ortolang.teicorpo.TeiCorpo -to CLAN .\aline1_gan.tei_corpo3.tei_corpo_ttg.tei_corpo.xml -normalize clan -o .\aline1_gan.tei_corpo4.cha
