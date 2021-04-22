0a74b4522090c71917ee0d4c2ce973d67538930f
Christophe Parisse <christopheparisse@gmail.com>
2021-04-22 12:38:33 +0200

    Prise en compte des fréquences + développement Stanza

--- tcof/ttg-nouveauxnoms.sh
+++ tcof/ttg-nouveauxnoms.sh
@@ -10,10 +10,10 @@ java -cp /devlopt/teicorpo/teicorpo.jar fr.ortolang.teicorpo.TeiCorpo -to CLAN N
 # génère des fichiers tei_corpo avec des lignes nettoyées (suite à la conversion vers CLAN)
 rm -rf NouveauxNoms/tei_corpo3
 mkdir NouveauxNoms/tei_corpo3
-java -cp /devlopt/teicorpo/teicorpo.jar fr.ortolang.teicorpo.TeiCorpo NouveauxNoms/clan -o NouveauxNoms/tei_corpo3 | tee erreurs_conversion.txt
+java -cp /devlopt/teicorpo/teicorpo.jar fr.ortolang.teicorpo.TeiCorpo NouveauxNoms/clan -o NouveauxNoms/tei_corpo3 | tee erreurs_conversion.log
 
 # génère l'analyse en parties du discours TTG
-java -cp /devlopt/teicorpo/teicorpo.jar fr.ortolang.teicorpo.TeiTreeTagger -syntaxformat ref -program /brainstorm/syntax/bin/tree-tagger -model /brainstorm/syntax/models/french-spoken.par NouveauxNoms/tei_corpo3 -target ttg | tee erreurs_ttg_perceo.txt
+java -cp /devlopt/teicorpo/teicorpo.jar fr.ortolang.teicorpo.TeiTreeTagger -syntaxformat ref -program /brainstorm/syntax/bin/tree-tagger -model /brainstorm/syntax/models/french-spoken.par NouveauxNoms/tei_corpo3 -target ttg | tee erreurs_ttg_perceo.log
 
 # move the results to a new directory
 rm -rf NouveauxNoms/ttg_perceo
