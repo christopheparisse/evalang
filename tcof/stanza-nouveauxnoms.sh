904a72099538f4261eddc5c7a6cfc7b71ce8e889
Christophe Parisse <christopheparisse@gmail.com>
2021-04-22 08:25:14 +0200

    Prise en compte des fréquences + développement Stanza

--- tcof/stanza-nouveauxnoms.sh
+++ tcof/stanza-nouveauxnoms.sh
@@ -6,3 +6,13 @@ mkdir NouveauxNoms/text
 java -cp /devlopt/teicorpo/teicorpo.jar fr.ortolang.teicorpo.TeiCorpo NouveauxNoms/tei_corpo3 -normalize clan -o NouveauxNoms/text -to text -n 1 -raw -tiernames -tierxmlid
 
 # compute results from stanza
+find NouveauxNoms/text -name "*txt" -exec python3 ../exemples_tei/stanza/text_oral_complex.py {} \;
+
+# move the results to a new directory
+rm -rf NouveauxNoms/conllu
+mkdir NouveauxNoms/conllu
+mv NouveauxNoms/text/*conllu NouveauxNoms/conllu
+
+rm -rf NouveauxNoms/conllu_clan
+mkdir NouveauxNoms/conllu_clan
+mv NouveauxNoms/text/*conllu.cex NouveauxNoms/conllu_clan
--- tcof/ttg-nouveauxnoms.sh
+++ tcof/ttg-nouveauxnoms.sh
@@ -13,6 +13,14 @@ mkdir NouveauxNoms/tei_corpo3
 java -cp /devlopt/teicorpo/teicorpo.jar fr.ortolang.teicorpo.TeiCorpo NouveauxNoms/clan -o NouveauxNoms/tei_corpo3 | tee erreurs_conversion.txt
 
 # génère l'analyse en parties du discours TTG
+java -cp /devlopt/teicorpo/teicorpo.jar fr.ortolang.teicorpo.TeiTreeTagger -syntaxformat ref -program /brainstorm/syntax/bin/tree-tagger -model /brainstorm/syntax/models/french-spoken.par NouveauxNoms/tei_corpo3 -target ttg | tee erreurs_ttg_perceo.txt
+
+# move the results to a new directory
 rm -rf NouveauxNoms/ttg_perceo
 mkdir NouveauxNoms/ttg_perceo
-java -cp /devlopt/teicorpo/teicorpo.jar fr.ortolang.teicorpo.TeiTreeTagger -syntaxformat ref -program /brainstorm/syntax/bin/tree-tagger -model /brainstorm/syntax/models/french-spoken.par NouveauxNoms/tei_corpo3 -target ttg | tee erreurs_ttg_perceo.txt
+mv NouveauxNoms/tei_corpo3/*tei_corpo_ttg.tei_corpo.xml NouveauxNoms/ttg_perceo
+
+rm -rf NouveauxNoms/ttg_perceo_divers
+mkdir NouveauxNoms/ttg_perceo_divers
+mv NouveauxNoms/tei_corpo3/*conll NouveauxNoms/ttg_perceo_divers
+mv NouveauxNoms/tei_corpo3/*vrt NouveauxNoms/ttg_perceo_divers
